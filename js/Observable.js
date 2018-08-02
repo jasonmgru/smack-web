de7aeba40953fc7279eacb2f506f438721bb9d94
Jason Gruenhagen <jasonmgru@gmail.com>
2018-08-01 20:30:24 -0500

    added Observable and ObservableList

---
+++ js/Observable.js
@@ -0,0 +1,88 @@
+/**
+ * Class which holds data and also notifies any observing functions
+ * of a data change.
+ */
+class Observable {
+
+    /**
+     * Adds a function to the observers list. Whenever the data in this object is 
+     * updated, all observers are called with the new value passed in as a
+     * parameter.
+     * 
+     * @param {(*) => void} observer The observer to be added.
+     */
+    subscribe(observer) {
+        this.observers.push(observer);
+    }
+
+    /**
+     * Removes a function from the observers list.
+     * 
+     * @param {(*) => void} observer The observer to be removed.
+     */
+    unsubscribe(observer) {
+        var index = this.observers.indexOf(observer);
+        if (index > -1) this.observers.splice(index, 1);
+    }
+
+    /**
+     * Sets the value of the data and notifies observers.
+     * 
+     * @param {*} newValue The new value for the data.
+     */
+    set(newValue) {
+        if (newValue !== this.value) {
+            this.value = newValue;
+            this.observers.forEach((subscriber) => subscriber(this.value));
+        }
+    }
+
+    constructor(value) {
+        this.value = value;
+        this.observers = [];
+    }
+}
+
+/**
+ * Class which holds lists of data and also notifies any observing functions
+ * of a data change.
+ */
+class ObservableList {
+
+    /**
+     * Adds a function to the observers list. Whenever the data in this object is 
+     * updated, all observers are called with the new value passed in as a
+     * parameter.
+     * 
+     * @param {(*, number) => void} observer The observer to be added.
+     */
+    subscribe(observer) {
+        this.observers.push(observer);
+    }
+
+    /**
+     * Removes a function from the observers list.
+     * 
+     * @param {(*, number) => void} observer The observer to be removed.
+     */
+    unsubscribe(observer) {
+        var index = this.observers.indexOf(observer);
+        if (index > -1) this.observers.splice(index, 1);
+    }
+
+    /**
+     * Pushes a new value to the list and notifies observers of the index of the change.
+     * 
+     * @param {*} newValue The new value for the data.
+     */
+    push(newValue) {
+        this.value.push(newValue);
+        this.observers.forEach((subscriber) => subscriber(newValue, this.value.length-1));
+        console.log(newValue);
+    }
+
+    constructor() {
+        this.value = [];
+        this.observers = [];
+    }
+}
