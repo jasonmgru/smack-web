6312ed910ff9a0816536b2fe111e4b03b5e5966f
Jason Gruenhagen <jasonmgru@gmail.com>
2018-08-02 20:23:52 -0500

    added new observable map

--- js/Observable.js
+++ js/Observable.js
@@ -58,7 +58,7 @@ class ObservableList {
     subscribe(observer) {
         this.observers.push(observer);
         this.value.forEach( (value, index) => {
-            observer(value, index);
+            observer(index, value);
         });
     }
 
@@ -79,11 +79,57 @@ class ObservableList {
      */
     push(newValue) {
         this.value.push(newValue);
-        this.observers.forEach((observer) => observer(newValue, this.value.length-1));
+        this.observers.forEach((observer) => observer(this.value.length-1, newValue));
     }
 
     constructor() {
         this.value = [];
+        this.observers = [];
+    }
+}
+
+/**
+ * Class which holds assiciative maps of data and also notifies any observing functions
+ * of a data change.
+ */
+class ObservableMap {
+    /**
+     * Adds a function to the observers list. Whenever the data in this object is 
+     * updated, all observers are called with the new value passed in as a
+     * parameter.
+     * 
+     * @param {(*, number) => void} observer The observer to be added.
+     */
+    subscribe(observer) {
+        this.observers.push(observer);
+        Object.entries(this.value).forEach(([key, value]) => {
+            observer(key, value);
+        });
+    }
+
+    /**
+     * Removes a function from the observers list.
+     * 
+     * @param {(*, number) => void} observer The observer to be removed.
+     */
+    unsubscribe(observer) {
+        var index = this.observers.indexOf(observer);
+        if (index > -1) this.observers.splice(index, 1);
+    }
+
+    /**
+     * Pushes a new value to the map at specified key and notifies observers of the index of the change.
+     * 
+     * @param {*} newKey The key to push to.
+     * @param {*} newValue The new value for the data.
+     */
+    add(newKey, newValue) {
+        this.value[newKey] = newValue;
+        this.observers.forEach((observer) => observer(newKey, newValue));
+    }
+
+    constructor() {
+        this.value = {};
         this.observers = [];
     }
 }
